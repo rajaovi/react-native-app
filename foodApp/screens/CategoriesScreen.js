436dd3768ac4dd71d0723cf964d2f37136dec082
Raja <orajacse@gmail.com>
2024-09-17 11:09:28 +0530

    Added Dummy Data & Create Card List

---
+++ foodApp/screens/CategoriesScreen.js
@@ -0,0 +1,24 @@
+import { FlatList, View } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTitle from "../components/CategoryGridTitle";
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTitle
+      title={itemData.item.title}
+      color={itemData.item.color}
+    />
+  );
+}
+
+function CategoriesScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+    />
+  );
+}
+export default CategoriesScreen;
